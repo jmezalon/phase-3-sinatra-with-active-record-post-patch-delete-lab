34521ae9b032a2560ff365119025f2ed348f4599
Max Mezalon <jmezalon@gmail.com>
2021-12-05 20:54:17 -0500

    all the routes are working and ready for the frontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
 # notebooks request
 
   get '/:user_id/notebooks' do
-    notebooks = Notebook.where('user-id=?', params[:user_id])
+    notebooks = Notebook.where('user_id=?', params[:user_id])
     notebooks.to_json
   end
 
@@ -45,7 +45,7 @@ class ApplicationController < Sinatra::Base
     notebook.to_json
   end
 
-  delete '/notebook/:id' do 
+  delete '/notebooks/:id' do 
     notebook = Notebook.find(params[:id])
     notebook.destroy
     notebook.to_json
@@ -83,10 +83,10 @@ class ApplicationController < Sinatra::Base
   end
 
   ## for switching notebook
-  patch '/:notebook_id' do
-    note = Note.find_by(notebook_id: params[:notebook_id])
-    note.update(notebook_id: params[:notebook_id])
-  end
+  # patch '/notebook/notes/:id' do
+  #   note = Note.find(params[:id])
+  #   note.update(notebook_id: params[:notebook_id])
+  # end
   # ***********************
 
   delete '/notes/:id' do 
@@ -94,44 +94,4 @@ class ApplicationController < Sinatra::Base
     note.destroy
     note.to_json
   end
-  
-#   get '/bakeries/:id' do
-#     bakery = Bakery.find(params[:id])
-#     bakery.to_json(include: :baked_goods)
-#   end
-
-#   get '/baked_goods/by_price' do
-#     # see the BakedGood class for the  method definition of `.by_price`
-#     baked_goods = BakedGood.by_price
-#     baked_goods.to_json
-#   end
-
-#   get '/baked_goods/most_expensive' do
-#     # see the BakedGood class for the  method definition of `.by_price`
-#     baked_good = BakedGood.by_price.first
-#     baked_good.to_json
-#   end
-
-#   post '/baked_goods' do
-#     baked_good = BakedGood.create(
-#       name: params[:name],
-#       price: params[:price],
-#       bakery_id: params[:bakery_id]
-#     )
-#     baked_good.to_json
-#   end
-
-#   patch '/bakeries/:id' do
-#     bakery = Bakery.find(params[:id])
-#     bakery.update(
-#       name: params[:name]
-#     )
-#     bakery.to_json
-#   end
-
-#   delete '/baked_goods/:id' do 
-#     baked_good = BakedGood.find(params[:id])
-#     baked_good.destroy
-#     baked_good.to_json
-#   end
 end
